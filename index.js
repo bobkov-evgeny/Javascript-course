e739807844d93baaba6605a9906cd3bc7c984784
Evgeny_Bobkov <freeedayy@gmail.com>
2021-07-14 18:56:28 +1000

    lesson 7 Debug

--- index.js
+++ index.js
@@ -274,4 +274,49 @@
 
 // Дополнительное задание
 
-const password = prompt("Введите пароль");
+// Задание 1
+
+// let health = prompt('Введите число параметра "здоровье" для персонажа');
+
+// if (health <= 0 || !health) {
+// 	alert('Параметр "здоровье" должен быть больше нуля!');
+// } else {
+// 	//console.log(Health);
+// 	alert(`Параметр "здоровье" равен ${health}`);
+// }
+
+// Задание 2
+
+// const temperatureInCelcius = prompt("Введите температуру в градусах Цельсия");
+
+// console.log(temperatureInCelcius, typeof temperatureInCelcius);
+
+// if (Number(temperatureInCelcius) === 0) {
+// 	alert("0 градусов по Цельсию - это температура замерзания воды.");
+// } else if (Number(temperatureInCelcius) < 0) {
+// 	alert(
+// 		"Для замерзания воды температура должна быть 0 градусов по Цельсию либо ниже."
+// 	);
+// }
+
+// //console.log(temperatureIncelcius);
+// const temperatureInFahrenheit = (temperatureInCelcius * 9) / 5 + 32;
+// alert(
+// 	`${temperatureInCelcius} градусов по Цельсию - это ${temperatureInFahrenheit} по Фаренгейту.`
+// );
+
+// Задание 3
+
+const salaryOfJuniorDeveloper = 500;
+const numberOfJuniorDevelopers = 3;
+let tapPercentage = 13;
+let totalJuniorDeveloperSalary = 0;
+
+for (let i = 0; i < numberOfJuniorDevelopers; i += 1) {
+	console.log(totalJuniorDeveloperSalary);
+
+	const salaryWithTax =
+		salaryOfJuniorDeveloper - (salaryOfJuniorDeveloper * tapPercentage) / 100;
+	totalJuniorDeveloperSalary += salaryWithTax;
+}
+console.log("totalJuniorDeveloperSalary", totalJuniorDeveloperSalary);
